5d43e82cf8524749bfcf4d8402bd58710bf2f199
Veronica P <vhayami@gmail.com>
2023-07-06 20:00:07 +0300

    fix: validation form after submit

--- src/components/BaseButton/BaseButton.vue
+++ src/components/BaseButton/BaseButton.vue
@@ -62,7 +62,7 @@ export default {
           button.classList.remove('active');
         }
 
-        if (this.text && this.text !== 'Выход') {
+        if (this.text) {
           this.changeActivePage(this.text);
         }
 
