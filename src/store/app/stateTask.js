2b3ff60e1e2e5927b208061e2d1647958bfd99a5
Veronica P <vhayami@gmail.com>
2023-07-15 23:49:26 +0300

    refactor filter and task page

--- src/store/app/stateTask.js
+++ src/store/app/stateTask.js
@@ -1,79 +1,50 @@
 import {useAxios} from '@/api/api';
 
 export const mutation = {
-
-  SET_TASKCURRENT: 'SET_TASKCURRENT',
   UPDATE_TASKLIST: 'UPDATE_TASKLIST',
-  SET_TASKTOTAL: 'SET_TASKTOTAL',
-  SET_TASKSORT: 'SET_TASKSORT',
-  SET_TASKSORTTYPE: 'SET_TASKSORTTYPE',
-  SET_TASKSEARCH: 'SET_TASKSEARCH',
+  UPDATE_TASKSETTING: 'UPDATE_TASKSETTING'
 }
 
 export default {
   state() {
     return {  
-      currentPage: 1,
       list: [],
-      totalPage: '',
-      sort: 'dateCreated',
-      sort_type: 'desc',
-      filter: {},
+      setting: {
+        page: 1,
+        totalPage: '',
+        sort: {
+          field: 'dateCreated',
+          type: 'desc',
+        },
+        filter: {},
+      }
     }
   },
 
   getters: {
-    taskCurrent: state => state.currentPage,
+    taskCurrent: state => state.setting.currentPage,
     taskList: state => state.list[0],
-    taskTotal: state => state.totalPage,
+    taskTotal: state => state.setting.totalPage,
   },
 
   mutations: {
-    [mutation.SET_TASKCURRENT]: (state, payload) => {
-      state.currentPage = payload
-    },
     [mutation.UPDATE_TASKLIST]: (state, payload) => {
       state.list = [payload]
     },
-    [mutation.SET_TASKTOTAL]: (state, payload) => {
-      state.totalPage = payload
+    [mutation.UPDATE_TASKSETTING]: (state, payload) => {
+      state.setting = payload;
     },
-    [mutation.SET_TASKSORT]: (state, payload) => {
-      state.sort = payload
-    },
-    [mutation.SET_TASKSORTTYPE]: (state, payload) => {
-      state.sort_type = payload
-    },
-    [mutation.SET_TASKSEARCH]: (state, payload) => {
-      state.filter = payload
-    }
   },
 
   actions: {
-    setTaskCurrent: ({dispatch, commit}, value) => {
-      commit(mutation.SET_TASKCURRENT, value)
-    },
-
-    setTaskSort: ({dispatch, commit}, value) => {
-      commit(mutation.SET_TASKSORT, value)
-    },
-
-    setTaskSortType: ({dispatch, commit}, value) => {
-      commit(mutation.SET_TASKSORTTYPE, value)
-    },
-
-    setTaskSearch: ({dispatch, commit}, value) => {
-      commit(mutation.SET_TASKSEARCH, value)
+    setTaskSetting: ({dispatch, commit}, value) => {
+      commit(mutation.UPDATE_TASKSETTING, value)
     },
 
     updateTaskList: async ({dispatch, commit, state, getters}, payload) => {
-      const curr = state.currentPage;
-      const sort = state.sort;
-      const sortType = state.sort_type;
-      const filterObj = state.filter;
+      const setting = state.setting;
 
-      const list = await useAxios('POST', `tasks/search`, {page: curr, filter: filterObj, sort: {field: sort, type: sortType}});
-      commit(mutation.SET_TASKTOTAL, list.total);
+      const list = await useAxios('POST', `tasks/search`, setting);
       commit(mutation.UPDATE_TASKLIST, list.tasks);
     },
   },
